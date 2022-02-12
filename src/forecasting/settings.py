8e0bc8718fa671c5d475a8ffb5d1b9ac8f842123
Mekarsa Base <mekarsa@sugengdcahyo.com>
2022-02-12 08:40:50 +0700

    reconfig and cors handling

--- src/forecasting/settings.py
+++ src/forecasting/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%ch@exx2&(tf0abecnpw*gs-4s*bmf@wofpxw&4u@hq*o5jd(k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # 3rd party,
+    'corsheaders',
     'django_extensions',
     'rest_framework',
     # apps
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'forecasting.urls'
 
