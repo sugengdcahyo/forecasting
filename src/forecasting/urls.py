4e611db2cd2033bc7bd1292d881745d9482299b9
Mekarsa Base <mekarsa@sugengdcahyo.com>
2022-02-13 01:10:27 +0700

    config static files and urls

--- src/forecasting/urls.py
+++ src/forecasting/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from services.router import service_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(service_urls), name='service_router')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
